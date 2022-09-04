ce5ebfc8be40b9ae50090b09112c22d9eaf2aee7
211 <lolbesttf@gmail.com>
2022-09-04 11:05:22 +0500

    example 05

---
+++ example 05/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n<100 || n > 999)
+{
+    Console.WriteLine("Неверное число, повторите. ");
+    n = Convert.ToInt32(Console.ReadLine());
+}    
+n = (n % 100) - (n % 10);
+n = n / 10;
+Console.Write(n);
+
