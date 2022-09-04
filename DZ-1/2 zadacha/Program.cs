501f98390e2c71a37ef29d039accc4bca46fb628
211 <lolbesttf@gmail.com>
2022-09-04 11:46:32 +0500

    zadacha 4 iz DZ

---
+++ DZ-1/2 zadacha/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (a >= b)
+max = a;
+else
+max = b;
+if (max <= c)
+{   max = c;
+    Console.WriteLine(max);
+}
+else
+Console.WriteLine(max);
