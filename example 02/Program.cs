8ab7e3ae0f6cc31a3b050e7284b8ef42b751345c
211 <lolbesttf@gmail.com>
2022-09-04 11:04:44 +0500

    example 02

---
+++ example 02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b*b == a)
+{
+    Console.WriteLine("Является");
+}
+else
+{
+    Console.WriteLine("Не является");
+
+}
