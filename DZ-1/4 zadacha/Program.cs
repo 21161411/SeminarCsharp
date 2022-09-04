1881ff639f087b1d0a4742b64242e2a24c719d2f
211 <lolbesttf@gmail.com>
2022-09-04 11:47:44 +0500

    zadacha 8 iz DZ

---
+++ DZ-1/4 zadacha/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1)
+{
+    Console.WriteLine("Неверное число, введите другое:");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 1; i <= n; i++)
+{
+    if (i%2 <= 0) 
+        Console.Write(i + " ");
+    else
+        Console.Write(" ");
+}
