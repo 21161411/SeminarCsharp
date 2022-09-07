8bc23ba2aef8f9960164e59bacd8904b6bf7794e
211 <lolbesttf@gmail.com>
2022-09-07 18:15:59 +0500

    2 zadacha reshena

---
+++ DZ-2/2 zadacha/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+void Numbers()
+{
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n >= 100) Console.WriteLine(Convert.ToString(n)[2]);
+    else
+    Console.WriteLine("Третьей цифры нет");
+}   
+
+Numbers();
