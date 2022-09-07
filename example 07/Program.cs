ab5a394075054219664ddcebf8bbc0fbb519b16e
211 <lolbesttf@gmail.com>
2022-09-07 18:14:34 +0500

    example 07

---
+++ example 07/Program.cs
@@ -0,0 +1,24 @@
+﻿int Generator()
+{
+    int n = new Random().Next(100,1000);
+    return n;
+}
+
+void NumberModification()
+{
+    int n = Generator();
+    Console.WriteLine(n);
+
+    //int a = n / 100;
+   // int b = n % 10;
+
+    //Console.WriteLine(a*10 + b);
+
+    //Альтернатива
+    Console.Write(Convert.ToString(n)[0]);
+    Console.Write(Convert.ToString(n)[2]);
+    
+
+}
+
+NumberModification();
