b29cff49e36670e0db7d3ff6750dba7dffc4c9ce
211 <lolbesttf@gmail.com>
2022-09-07 18:14:24 +0500

    example 06

---
+++ example 06/Program.cs
@@ -0,0 +1,21 @@
+﻿int GenerateRandomNumbers()
+{
+    int n = new Random().Next(10,100);
+    return n;
+}
+
+void CompraiseNumbers()
+{
+    int n = GenerateRandomNumbers();
+    Console.WriteLine(n);
+    int a = n / 10;
+    int b = n % 10;
+    if (a > b)
+    Console.WriteLine(a);
+    else if (a < b)
+    Console.WriteLine(b);
+    else
+    Console.WriteLine("=");
+}
+
+CompraiseNumbers();
