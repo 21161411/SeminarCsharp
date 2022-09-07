beeefa7ec58e04b000615b085e36d673fb85b66f
211 <lolbesttf@gmail.com>
2022-09-07 18:14:41 +0500

    example 08

---
+++ example 08/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Clear();
+void InputNumbers()
+{
+    Console.Write("Напишите первое число: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Напишите второе число: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(a + " " + b);
+    if (a % b == 0)
+        Console.WriteLine("Кратно");
+    else
+        Console.WriteLine("Остаток " + a%b);
+}
+
+InputNumbers();
