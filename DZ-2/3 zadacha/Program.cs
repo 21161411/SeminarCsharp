c5812f1e1ab503b425a26e26417795293afbba66
211 <lolbesttf@gmail.com>
2022-09-07 18:16:34 +0500

    3 zadacha reshena

---
+++ DZ-2/3 zadacha/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+void TypeOfDay()
+{
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n < 1 || n > 7) Console.WriteLine("Неверное число. ");
+    else if (n < 6 )
+    Console.WriteLine("Рабочий день");
+    else if (n > 5 )
+    Console.WriteLine ("Выходной день");
+
+}
+
+TypeOfDay();
