12e23ce3043d5b6aa6b01a5bda921b1dfc8c10a0
211 <lolbesttf@gmail.com>
2022-09-07 18:14:48 +0500

    example 09

---
+++ example 09/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+void Numbers()
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    if (a%7 == 0 && a%23 == 0)
+        Console.WriteLine("Кратно");
+    else
+    Console.WriteLine("Не кратно");
+
+    
+
+
+}
+
+Numbers();
