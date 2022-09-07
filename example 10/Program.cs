dbda5ced62e3e505e8730b9e9c9def67968ed559
211 <lolbesttf@gmail.com>
2022-09-07 18:15:02 +0500

    example 10

---
+++ example 10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+void Numbers()
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    if (a*a == b || b*b == a)
+        Console.WriteLine("Да, являются. ");
+    else
+        Console.WriteLine("Нет, не являются");
+
+
+
+}
+
+Numbers();
