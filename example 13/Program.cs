f609c2adc68eeb9a024fe391f644b0c695fa5dd7
211 <lolbesttf@gmail.com>
2022-09-09 17:08:22 +0500

    added example 13

---
+++ example 13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите X1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите X2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+void Sum()
+{
+    double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+    Console.WriteLine(result);
+}
+
+Sum();
+
