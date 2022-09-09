e257f6fac9d1b8e9075d87563190fd1421af7ff5
211 <lolbesttf@gmail.com>
2022-09-09 17:08:53 +0500

    Reshil 1 zadachu

---
+++ DZ-3/1 zadacha/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int oldValue = number;
+int newValue = 0;
+if (number >= 10000 && number < 100000)
+{
+    while (number > 0)
+    {
+        int dig = number % 10;
+        newValue = newValue * 10 + dig;
+        number = number / 10;
+    }
+    if (newValue == oldValue)Console.WriteLine("Является. ");
+    else Console.WriteLine("Не является. ");
+}    
+else Console.WriteLine ("Неверное число. ");
