c7ba8af08c0eefb0fe29ac5718e18775d092cfd4
211 <lolbesttf@gmail.com>
2022-09-09 17:08:12 +0500

    added example 11

---
+++ example 11/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите координаты X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x> 0 && y > 0) Console.WriteLine("Первая четверть");
+else if (x < 0 && y > 0) Console.WriteLine("Вторая четверть");
+else if (x > 0 && y < 0) Console.WriteLine("Четвертая четверть");
+else if (x < 0 && y < 0) Console.WriteLine("Третья четверть");
