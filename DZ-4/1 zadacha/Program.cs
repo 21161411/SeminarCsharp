0a08d03f35a7670f51600050715d7fcccc37bdaf
211 <lolbesttf@gmail.com>
2022-09-15 21:35:04 +0500

    1 zadacha DZ-4

---
+++ DZ-4/1 zadacha/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(Math.Pow(a, b));
+int result = a;
+int i = 1;
+while(i < b)
+{
+    result *= a;
+    i++;
+}
+
+Console.WriteLine(result);
+
+
+
