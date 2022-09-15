ab7333a67a02f278cec72f671be5c4730567d78a
211 <lolbesttf@gmail.com>
2022-09-15 21:35:28 +0500

    3 zadacha DZ-4

---
+++ DZ-4/3 zadacha/Program.cs
@@ -0,0 +1,20 @@
+﻿int [] array = new int[8];
+
+for(int i = 0; i < 8; i++)
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    array[i] = a;
+
+}
+
+//Console.Write("Введите количество элементов массива: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+//int[] array = new int[n];
+// for(int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(0, 100);
+
+// }
+
+ Console.WriteLine("[" + string.Join(", ", array) + "]");
+
