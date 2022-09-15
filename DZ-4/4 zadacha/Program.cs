53c69d1c4819a30c2027fd897afe2e5bf15e6b90
211 <lolbesttf@gmail.com>
2022-09-15 21:56:16 +0500

    bonus task fix

--- DZ-4/4 zadacha/Program.cs
+++ DZ-4/4 zadacha/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Сколько посадили кустов: ");
+﻿Console.Write("Сколько посадили кустов: ");
 
 int numbers = Convert.ToInt32(Console.ReadLine());
 while(numbers < 3)
@@ -9,20 +9,29 @@ while(numbers < 3)
 }
 //int numbers = new Random().Next(3, 1001);
 int[] kusti = new int[numbers];
-int sum = 0;
-for(int i = 0; i < kusti.Length; i++)
-{
-    kusti[i] = new Random().Next(0,100);
-    sum = sum + kusti[i];
-    if(sum >= 1000) break;
+//int sum = 0;
+//for(int i = 0; i < kusti.Length; i++)
+// {
+//     kusti[i] = new Random().Next(0,100);
+//     sum = sum + kusti[i];
+//     if(sum >= 1000) break;
+
+// }
+// Console.WriteLine(string.Join(" ", kusti));
+for (int i = 0; i < numbers; i++)
+{   
+    Console.Write("Количество ягод на " + (i+1) + " кусте: ");
+    kusti[i] = Convert.ToInt32(Console.ReadLine());
 
 }
+Console.WriteLine();
 Console.WriteLine(string.Join(" ", kusti));
-
 int max = 0;
 for(int j = 1; j < kusti.Length -1 ; j++)
 {
     int max2 = kusti[j-1] + kusti[j] + kusti[j+1];
     if (max < max2) max = max2;
 }
-Console.WriteLine(max);
+Console.WriteLine();
+
+Console.WriteLine("Рекорд сбора: " + max);
