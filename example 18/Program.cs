5be889fd6cc27d67a21488f71a22bc246e28c37e
211 <lolbesttf@gmail.com>
2022-09-15 21:34:13 +0500

    Example 18

---
+++ example 18/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] array = new int[8];
+
+void FillArray(int[] array)
+{
+    int i = 0;
+    while(i < array.Length)
+    {
+        array[i] = new Random().Next(0,2);
+        i++;
+    }
+}
+
+FillArray(array);
+
+void PrintArray(int[] arr)
+{
+    int position = 0;
+    while(position < arr.Length)
+    {
+        Console.Write(arr[position] + " ");
+        position++;
+    }
+
+
+}
+
+//PrintArray(array);
+
+Console.WriteLine("[" + string.Join(", ", array) + "]");
