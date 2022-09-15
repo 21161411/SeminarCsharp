4be3f9a643e82cbe05ab36e33fb30df5839d9186
211 <lolbesttf@gmail.com>
2022-09-15 15:36:44 +0500

    razobrali na seminare dop zada4u

---
+++ DZ-3/4 zadacha/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+Console.WriteLine();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+
+}
+Console.WriteLine();
+int count = 0;
+int countMax = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0) count++;
+
+    else if(array[i] < 0) count = 0;
+
+    if(countMax < count) countMax = count;
+
+}
+Console.WriteLine(countMax); 
