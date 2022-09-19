ee871908b88ea5044456cc4ca34aaa740c24a798
211 <lolbesttf@gmail.com>
2022-09-19 22:42:37 +0500

    3 zadacha

---
+++ DZ-5/3 zadacha/Program.cs
@@ -0,0 +1,33 @@
+﻿void Fillarray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round(new Random().NextDouble() + new Random().Next(-100,100),3);
+    }
+
+}
+
+double[] arr = new double[10];
+Fillarray(arr);
+Console.WriteLine("[" + string.Join(", ", arr) + "]");
+double FindDiff(double[] array)
+{
+    double max = array[0];
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max) max = array[i];
+
+    }
+    
+    double min = array[0];
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < min) min = array[i];
+
+    }
+    double dif = max - min;
+    return dif;
+}
+
+Console.WriteLine(FindDiff(arr));
+
