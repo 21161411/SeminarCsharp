4f0ed037fc527eaeef246494767328799917595f
211 <lolbesttf@gmail.com>
2022-09-19 22:42:25 +0500

    2 zadacha

---
+++ DZ-5/2 zadacha/Program.cs
@@ -0,0 +1,25 @@
+﻿void Fillarray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+
+}
+int[] arr = new int[10];
+Fillarray(arr);
+Console.WriteLine("[" + string.Join(", ", arr) + "]");
+int Sum(int[] array)
+{
+    int sum = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i%2 > 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+Console.WriteLine(Sum(arr));
