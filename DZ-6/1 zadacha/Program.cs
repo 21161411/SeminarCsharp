28bb1ac0e8816fe7091369f41a2fc54b81c38307
211 <lolbesttf@gmail.com>
2022-09-22 23:44:14 +0500

    reshil 1 zadachu

---
+++ DZ-6/1 zadacha/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for(int i = 1; i <= n; i++)
+{
+    Console.Write("Введите число " + i +" : ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if(x > 0) count++;    
+
+}
+Console.WriteLine("Количество положительных чисел равно: " + count);
