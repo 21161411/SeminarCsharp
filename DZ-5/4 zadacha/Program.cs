c66d885f90032634eb1bf3683f6ff4f12fb12632
211 <lolbesttf@gmail.com>
2022-09-22 23:42:59 +0500

    vsyal reshenie dop.z. s seminara

---
+++ DZ-5/4 zadacha/Program.cs
@@ -0,0 +1,41 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int k = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int[] arrayResult = new int[n];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+
+}
+Console.WriteLine(string.Join(", ", array));
+
+k = k % n;
+if(k > 0)
+{
+    for(int i = 0; i < k; i++)
+    {
+        arrayResult[i] = array[array.Length - k + i];
+    }
+    Console.WriteLine(string.Join(", ", arrayResult));
+
+    for(int i = 0; i < n - k; i++)
+    {
+        arrayResult[k + i] = array[i];
+    }
+}
+else
+{
+    k = (-1) * k;
+    for(int i = 0; i < k; i++)
+    {
+        arrayResult[array.Length - k + i] = array[i];
+    }
+    Console.WriteLine(string.Join(", ", arrayResult));
+    
+    for(int i = 0; i < n - k; i++)
+    {
+        arrayResult[i] = array[k + i];
+    }
+}
+Console.WriteLine(string.Join(", ", arrayResult));
