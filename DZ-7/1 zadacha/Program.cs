89afafbbdc403c14149e9daf95defe3474d5fcb0
211 <lolbesttf@gmail.com>
2022-09-26 03:01:56 +0500

    1 zadacha

---
+++ DZ-7/1 zadacha/Program.cs
@@ -0,0 +1,28 @@
+﻿
+void FillArray (double[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0);i++)
+        for(int j = 0; j < arr.GetLength(1);j++)
+        {
+            arr[i,j] = Math.Round(new Random().NextDouble() + new Random().Next(-10,10),2);
+        }
+}
+
+void PrintArray(double[,] arr)
+{
+   for(int i = 0; i < arr.GetLength(0);i++)
+    {    for(int j = 0; j < arr.GetLength(1);j++)
+        {
+             Console.Write($"{arr[i,j]}" + "\t");
+        }
+        Console.WriteLine(); 
+    }
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[n,m];
+
+FillArray(array);
+PrintArray(array);
