669929a6e6e47b322561e6a6f5123d3b1a66bda3
211 <lolbesttf@gmail.com>
2022-09-26 03:02:10 +0500

    2 zadacha

---
+++ DZ-7/2 zadacha/Program.cs
@@ -0,0 +1,42 @@
+﻿void PrintArray(int[,] arr)
+{
+     for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]}" + "\t");
+        }
+    Console.WriteLine();   
+    }
+}
+
+void FillArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(-10,11);
+        }
+    }
+}
+
+int x = new Random().Next(2,6);
+int y = new Random().Next(2,6);
+int[,] array = new int[x,y];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+Console.Write("Укажите строку: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Укажите столбец: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if( n < array.GetLength(0))
+{
+    if( m < array.GetLength(1)) Console.WriteLine("Значение указанного элемента равно: " + array[n,m]);
+    else Console.WriteLine("Нет указанного элемента");
+} 
+else Console.WriteLine("Нет указанного элемента");
