ff6df370d8b7a61b0d65650c4935de1d8743cd68
211 <lolbesttf@gmail.com>
2022-10-11 01:09:19 +0500

    reshenie DZ

---
+++ DZ-9/1 zadacha/Program.cs
@@ -0,0 +1,17 @@
+﻿int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+//int sum = 0;
+// while(n <= m)
+// {
+//     sum += n;
+//     n++;
+// }
+// Console.WriteLine(sum);
+
+int PrintNumber(int n, int m)
+{
+    if(n == m - 1) return 0;
+    return PrintNumber(n - 1, m) + n;
+
+}
+Console.WriteLine(PrintNumber(n,m));
---
+++ DZ-9/2 zadacha/Program.cs
@@ -0,0 +1,10 @@
+﻿int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+int A(int m, int n)
+{
+    if(m == 0) return n + 1;
+    else if(m > 0 && n == 0) return A(m - 1, 1);
+    return A(m - 1, A(m, n - 1));
+}
+Console.WriteLine(A(m,n));
